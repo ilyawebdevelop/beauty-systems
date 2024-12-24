98543b9608526327940578b3894b450faae3d294
ilyawebdevelop <ilia966@tut.by>
2024-12-24 09:56:26 +0300

    oborud

--- src/js/app.js
+++ src/js/app.js
@@ -312,6 +312,17 @@ document.querySelectorAll('.teamSlider').forEach(n => {
       prevEl: n.querySelector('.navArrowPrev'),
       nextEl: n.querySelector('.navArrowNext'),
     },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,      
+      }, 
+      992: {
+        slidesPerView: 3,    
+      },  
+      1200: {
+        slidesPerView: 4,    
+      },  
+    },
   });
 });
 
